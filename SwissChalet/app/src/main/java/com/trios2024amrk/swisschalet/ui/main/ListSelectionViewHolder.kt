9f074b3dbaec0052b9e61b0ee12aa601509ad010
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-09-07 11:06:34 -0400

    End of ch 8

---
+++ SwissChalet/app/src/main/java/com/trios2024amrk/swisschalet/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,9 @@
+package com.trios2024amrk.swisschalet.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.trios2024amrk.swisschalet.databinding.FragmentMainBinding
+import com.trios2024amrk.swisschalet.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding):
+    RecyclerView.ViewHolder(binding.root) {
+}
---
+++ SwissChalet/app/src/main/java/com/trios2024amrk/swisschalet/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.trios2024amrk.swisschalet.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
